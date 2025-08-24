0f80c0281373c4244a67b907a7fece877ca77c1f
Zack-translator <Zack-translator@sebastianfiser2275@gmail.com>
2025-08-24 12:17:00 +0200

    placeholder pro systém

---
+++ installer/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# entry point boostrap-installer
+
+# první placeholder 
+def main():
+    print("Boostrap installer running......")
+
+if __name__ == "__main__":
+    main()
